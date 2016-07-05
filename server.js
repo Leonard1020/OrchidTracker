dabd95cc47dcdbd6e8e00f428eda8eaf77dacfde
unknown <leonardj@msoe.edu>
2016-07-05 15:53:56 -0500

    reverting to old commit

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require('express');
-var backend = require('./backend');
 app = express();
 app.use(express.static('www'));
 app.set('port', process.env.PORT || 45101);
 app.listen(app.get('port'), function () {
     console.log('Ionic server listening on port ' + app.get('port'));
-	backend.startBackend();
 });
