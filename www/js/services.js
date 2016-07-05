510e791292ee6e22e1c5bfd576880d8900924984
unknown <leonardj@msoe.edu>
2016-07-05 16:01:35 -0500

    added serverURL

--- www/js/services.js
+++ www/js/services.js
@@ -1,18 +1,20 @@
 angular.module('starter.services', [])
 
 .factory('Plots', function($http) {
+	var serverURL = "https://orchid-tracker-server.herokuapp.com";
+	
 	return {
 		all: function() {
-			return $http.get('/plots');
+			return $http.get(serverURL + '/plots');
 		},
 		get: function(id) {
-			return $http.get('/plots/' + id);
+			return $http.get(serverURL + '/plots/' + id);
 		},
 		getNumbers: function() {
-			return $http.get('/plots/numbers');
+			return $http.get(serverURL + '/plots/numbers');
 		},
 		put: function(plot, success, error) {
-			return $http.put('plots/' + plot.number, plot)
+			return $http.put(serverURL + 'plots/' + plot.number, plot)
 							.success(success)
 							.error(error);
 		}
