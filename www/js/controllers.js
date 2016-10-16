feb8c78fa6494104c6f48cd58e194f9c022562ad
unknown <leonardj@msoe.edu>
2016-10-16 09:19:51 -0500

    tried to add camera

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -261,7 +261,17 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('StatusCtrl', function($scope, Plots) {
+.controller('StatusCtrl', function($scope, Plots, AuthService) {
+	AuthService.authRequired().then(function(response) {
+		$scope.authRequired = false;
+		$scope.padding = {
+			top: 0,
+			bottom: 0
+		};
+	}).catch(function(err) {
+		$scope.authRequired = true;
+	});
+
 	Plots.get(localStorage.getItem('selectedPlot')).$loaded().then(function(plot) {
 		$scope.plot = plot;
 		if (!plot.plants) {
@@ -269,6 +279,26 @@ angular.module('starter.controllers', [])
 		}
 		$scope.plantCount = plot.plants.filter(getLivingPlants).length;
 	});
+
+	$scope.takePicture = function() {
+		var options = {
+	    quality : 75,
+	    destinationType : Camera.DestinationType.DATA_URL,
+	    sourceType : Camera.PictureSourceType.CAMERA,
+	    allowEdit : true,
+	    encodingType: Camera.EncodingType.JPEG,
+	    targetWidth: 300,
+	    targetHeight: 300,
+	    popoverOptions: CameraPopoverOptions,
+	    saveToPhotoAlbum: false
+    };
+
+    $cordovaCamera.getPicture(options).then(function(imageData) {
+    	$scope.imgURI = "data:image/jpeg;base64," + imageData;
+    }, function(err) {
+      // An error occured. Show a message to the user
+    });
+	}
 })
 
 .controller('UpdateCtrl', function($scope, $state, Plots, AuthService) {
